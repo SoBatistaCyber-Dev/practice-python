0e49307fcec3eecd315ded4bb8f275545d751423
SoBatista <solangefilipa08@gmail.com>
2024-03-24 16:28:03 +0100

    [KAN-128] - update of the rps game

--- python_lessons/rps.py
+++ python_lessons/rps.py
@@ -8,31 +8,35 @@ class RPS(Enum):
     PAPER = 2
     SCISSORS = 3
 
+while(True):
+    print("")
+    playerchoice = input("Enter...\n1 for Rock,\n2 for Paper or \n3 for Scissors:\n0 Exit\n")
 
-print("")
-playerchoice = input("Enter...\n1 for Rock,\n2 for Paper or \n3 for Scissors:\n\n")
+    player = int(playerchoice)
 
-player = int(playerchoice)
+    if player < 0 or player > 3:
+        sys.exit("You must enter 0, 1, 2 or 3.")
 
-if player < 1 or player > 3:
-    sys.exit("You must enter 1, 2 or 3.")
+    if player == 0:
+        print("Bye bye")
+        break
 
-computerchoice = random.choice("123")
+    computerchoice = random.choice("123")
 
-computer = int(computerchoice)
+    computer = int(computerchoice)
 
-print("")
-print("You chose " + str(RPS(player)).replace('RPS.','') + ".")
-print("Python chose " + str(RPS(computer)).replace('RPS.','') + ".")
-print("")
+    print("")
+    print("You chose " + str(RPS(player)).replace('RPS.','') + ".")
+    print("Python chose " + str(RPS(computer)).replace('RPS.','') + ".")
+    print("")
 
-if player == 1 and computer == 3:
-    print("You win!")
-elif player == 2 and computer == 1:
-    print("You win!")
-elif player == 3 and computer == 2:
-    print("You win!")
-elif player == computer:
-    print("Tie game!")
-else:
-    print("Python wins!")
+    if RPS(player) == RPS.ROCK and RPS(computer) == RPS.SCISSORS:
+        print("You win!")
+    elif RPS(player) == RPS.PAPER and RPS(computer) == RPS.ROCK:
+        print("You win!")
+    elif RPS(player) == RPS.SCISSORS and RPS(computer) == RPS.PAPER:
+        print("You win!")
+    elif player == computer:
+        print("Tie game!")
+    else:
+        print("Python wins!")
