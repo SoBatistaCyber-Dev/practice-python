e2db13ff11584d5e1915124f154487e2b5b30c8a
SoBatista <solangefilipa08@gmail.com>
2024-10-10 15:08:36 +0200

    [Close #4] - Solved 3 challenge

--- challenge_30_days/day_3.py
+++ challenge_30_days/day_3.py
@@ -12,3 +12,22 @@
 '''
 
 # Your solution goes here <- delete this 
+
+word = input(str("Enter a word: "))
+remove_characters = input(str("How many characters do you want to remove from the begining of your word? "))
+
+def remove_chars(word):
+    return word
+print("Your word will be:", word[int(remove_characters):])
+
+
+"""
+word_1 = "PYnative"
+word_2 = "PYnative" 
+
+def remove_chars(word_1, word_2):
+    return word_2, word_1
+
+print(word_1[4:], word_2[2:])
+
+"""
