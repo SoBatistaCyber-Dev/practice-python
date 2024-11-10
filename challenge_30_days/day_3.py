628b4da9162a44cfea1dafae68eef890e2cad22a
mregra <marregsil97@gmail.com>
2024-11-10 18:09:29 +0100

    Second attempt the challenge

--- challenge_30_days/day_3.py
+++ challenge_30_days/day_3.py
@@ -1,25 +1,11 @@
 '''
-    Remove first n characters from a string
+    Write a Python program that asks the user for the number of seconds and converts into into the number of days, hours and seconds corresponding to those seconds:
 
-    Write a Python code to remove characters from a string from 0 to n and return a new string.
-
-    For Example:
-
-        remove_chars("PYnative", 4) so output must be tive. Here, you need to remove the first four characters from a string.
-        remove_chars("PYnative", 2) so output must be native. Here, you need to remove the first two characters from a string.
-
-    Note: n must be less than the length of the string.
+    Example:
+    Write a number of seconds:
+    ? 345678
+        days: 4
+        hours: 0
+        mins: 1
+        secs: 18
 '''
-
-# Your solution goes here <- delete this 
-
-def remove_chars(word, remove_characters):
-    return word[int(remove_characters):]
-
-print("Your words will be:", remove_chars("PYnative", 4))
-print("Your words will be:", remove_chars("PYnative", 2))
-
-word_1 = input(str("Enter a word: "))
-remove_characters_1 = input(str("How many characters do you want to remove from the begining of your first word? "))
-
-print("Your words will be:", remove_chars(word_1, remove_characters_1))
