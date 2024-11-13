a67a1876c35538067cad843155737852e7cae3c6
SoBatista <solangefilipa08@gmail.com>
2024-11-13 11:30:28 +0100

    [Close #37] - Solved 1º exercise + code improvement

--- challenge_30_days/day_1.py
+++ challenge_30_days/day_1.py
@@ -1,23 +1,27 @@
 '''
     Write a Python program that asks the user for a test grade from 0 to 100 and prints the the grade in letter form:
-    A - 100-85
+    A -  85-100
     B -  66-84
     C -  50-65
     D -  25-49
     E -   0-24
 '''
 
-grade = int(input("What is your grade from 0 to 100? "))
 
-if grade in range(0, 25):
-    print("This is your letter form: E")
-elif grade in range(25, 50):
-    print("This is your letter form: D")
-elif grade in range(50, 66):
-    print("This is your letter form: C")
-elif grade in range(66, 85):
-    print("This is your letter form: B")
-elif grade in range(85, 101):
-    print("This is your letter form: A")
-else:
-    print("That does not exist!")
+
+def grade_number(grade):
+    if grade in range(0, 25):
+        print("You get an: E")
+    elif grade in range(25, 50):
+        print("You get an: D")
+    elif grade in range(50, 66):
+        print("You get an: C")
+    elif grade in range(66, 85):
+        print("You get an: B")
+    elif grade in range(85, 101):
+        print("You get an: A")
+    else:
+        print("Error: There is no such grade!")
+
+grade = int(input("What is your grade from 0 to 100? "))
+grade_number(grade)
