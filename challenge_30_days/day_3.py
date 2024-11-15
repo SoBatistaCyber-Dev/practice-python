5e5c4efe109d1498af33a5d8407192c9e79df0f9
SoBatista <72103679+SoBatista@users.noreply.github.com>
2024-11-15 10:47:16 +0100

    #38 solving 3º challenge (#45)
    * [Close #37] - Solved 1º Challenge
    
    * [Close #37] - Solved 1º exercise + code improvement
    
    * [Close #39] - Solved 2º challenge
    
    * [Close #38] - Solved 3º challenge

--- challenge_30_days/day_3.py
+++ challenge_30_days/day_3.py
@@ -9,3 +9,17 @@
         mins: 1
         secs: 18
 '''
+
+def seconds_converter(seconds_from_user):
+    days = seconds_from_user // 86400
+    hours = (seconds_from_user % 86400) // 3600
+    minutes = (seconds_from_user % 3600) // 60
+    seconds = (seconds_from_user % 60)
+    print("Your number of seconds corresponds to:")
+    print("Days:", days)
+    print("Hours:", hours)
+    print("Minutes:", minutes)
+    print("Seconds:", seconds)
+
+seconds_from_user = int(input("Enter the number of seconds you want to convert into days, hours, minutes, and seconds: "))
+seconds_converter(seconds_from_user)
