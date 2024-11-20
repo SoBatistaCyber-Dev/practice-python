2a5d731f6106c8c79809278b2e263d0ce049a9a7
SoBatista <solangefilipa08@gmail.com>
2024-11-20 10:09:51 +0100

    [Close #49] - 3º challenge done

--- challenge_30_days/day_3.py
+++ challenge_30_days/day_3.py
@@ -2,3 +2,20 @@
     Write a Python program to test whether a passed letter is a vowel or not. 
     
 """
+
+def vowel(letter):
+    if letter == "a":
+        print("That's a vowel!")
+    elif letter == "e":
+        print("That's a vowel!")
+    elif letter == "i":
+        print("That's a vowel!")
+    elif letter == "o":
+        print("That's a vowel!")
+    elif letter == "u":
+        print("That's a vowel!")
+    else:
+        print("That's not a vowel!")
+
+letter = str(input("Enter a letter to see if it's a vowel or not: "))
+vowel(letter)
