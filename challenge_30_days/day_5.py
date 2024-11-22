5dff9db13527f8774b64db95f94d38c911321a6b
SoBatista <solangefilipa08@gmail.com>
2024-11-22 21:21:31 +0100

    [Close #7] - 5º challenge done

--- challenge_30_days/day_5.py
+++ challenge_30_days/day_5.py
@@ -10,3 +10,14 @@
         It is a prime number
         Give me a number: ...
 '''
+
+def is_prime(number):
+    if number == 2 or number == 3:
+        return "It's a prime number!"
+    for i in range(2, number):
+        if number % i == 0:
+            return "It's not a prime number!"
+    return "It's a prime number!"
+
+number = int(input("Enter a number to see if it's a prime or not: "))
+print(is_prime(number))
