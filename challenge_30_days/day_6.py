d2329b6298143cedcaaa9577aa4b322a352b018b
SoBatista <solangefilipa08@gmail.com>
2024-11-23 10:28:07 +0100

    [Close #8] - Solved 6º challenge

--- challenge_30_days/day_6.py
+++ challenge_30_days/day_6.py
@@ -5,26 +5,20 @@ Exercise: Guess the Number Game
 Objective: Practice using loops, conditionals, random numbers, and user input in Python.
 Instructions:
 
-    Write a program where the computer picks a random number between 1 and 100.
-    The player has to guess the number, and the program gives hints:
-        If the guess is too high, display "Too high!"
-        If the guess is too low, display "Too low!"
+    Write a program where the computer picks a random number between 1 and 10.
+    The player has to guess the number:
     When the player guesses the correct number, display a congratulatory message and the number of attempts it took.
-    Include error handling to ensure the user enters a valid number.
 
 Example Output:
 
 Welcome to the Guess the Number Game!
 I'm thinking of a number between 1 and 100.
 
-Enter your guess: 50
-Too low! Try again.
+Enter your guess: 5
+Wrong guess. Try again!
 
-Enter your guess: 75
-Too high! Try again.
-
-Enter your guess: 62
-Congratulations! You guessed it in 3 attempts.
+Enter your guess: 2
+Congratulations! You guessed it in 2 attempts.
 
 Hints:
 
@@ -33,3 +27,22 @@ Hints:
     Use int() to convert input strings to numbers.
     Keep track of the number of attempts with a counter.
 """
+import random
+
+def random_guess_number():
+    print("------------------------------------------")
+    print("Welcome to the Guess the Number Game!")
+    print("I'm thinking of a number between 1 and 10.")
+    print("------------------------------------------")
+    random_number = random.randint(1, 10)
+    nr_of_guesses = 0 #Initialize the number of guesses
+    while True:
+        guess_number = int(input("Enter your guess: "))
+        nr_of_guesses += 1 # Increment the guess count
+        if guess_number == random_number:
+            print("Congratulations! You guessed it in", nr_of_guesses, "attempts")
+            break
+        else:
+            print("Wrong guess. Try again!")
+        
+random_guess_number()
