3a8ae91feec6d471a5ca714606fbb43e418fcf7a
SoBatista <72103679+SoBatista@users.noreply.github.com>
2024-11-25 10:04:59 +0100

    [Close #10] - 8º Challenge done (#57)

--- challenge_30_days/day_8.py
+++ challenge_30_days/day_8.py
@@ -11,3 +11,21 @@
     Give me a number: 125
     No. given number is not palindrome number
 '''
+
+def check_palindrome_number(number):
+    return number == number[::-1] 
+    # This will print the string backwords and see if it stays the same as the original. 
+    # And if it does it's because it's a palindrome number.
+    
+number = input("Give me a number: ")
+
+if check_palindrome_number(number):
+    #This checks if the result of calling this funtion with the 'number' statement is True or Flase.
+    # If it's True it prints the string below. 
+    print("Yes. The given number is palindrome number!")
+else:
+    print("No. The given number is not a palindrome number!")
+    # If it's False prints this one.
+
+
+check_palindrome_number(number)
