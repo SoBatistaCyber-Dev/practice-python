b0d08818b144999cc858361a76ee0e763c1ddd4a
SoBatista <72103679+SoBatista@users.noreply.github.com>
2024-11-27 20:14:46 +0100

    [Close #12] - 10 Challenge done (#59)
    * [Close #12] - 10 Challenge done
    
    * [Close #12] - 10 Challenge done with code improvement
    
    * [Close #12] - 10 Challenge done with code improvement

--- challenge_30_days/day_10.py
+++ challenge_30_days/day_10.py
@@ -13,3 +13,23 @@
 
     10000*0% + 10000*10%  + 25000*20% = $6000.
 '''
+
+def income_tax(income):
+    if income >= 10000:
+        income = income - 10000
+        first_bracket_tax = 10000 * 0
+        second_bracket_tax = 0
+        third_bracket_tax = 0
+        if income >= 10000:
+            income = income - 10000
+            second_bracket_tax = 10000 * 0.1    
+            if income > 0:
+                third_bracket_tax = income * 0.2
+            print("Your taxes are: $", int(first_bracket_tax + second_bracket_tax + third_bracket_tax))
+        else:    
+            print("Your taxes are: $", int(first_bracket_tax + (income * 0.1)))
+    else:
+        print("Your taxes are: $", int(income * 0))
+
+income = int(input("Enter your income: "))
+income_tax(income)
