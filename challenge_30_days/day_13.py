53627f0cb175390bee4395498c7a11892d516a52
SoBatista <72103679+SoBatista@users.noreply.github.com>
2024-11-30 12:13:07 +0100

    [Close #15] - 13 Challenge done (#62)

--- challenge_30_days/day_13.py
+++ challenge_30_days/day_13.py
@@ -5,3 +5,27 @@
     List : ['3', ' 5', ' 7', ' 23']
     Tuple : ('3', ' 5', ' 7', ' 23')
 '''
+import re
+import sys
+
+def check_if_ints(lst):
+    try:
+        for i in lst:
+            int(i)
+    except ValueError:
+        print("That's not a number!")
+        sys.exit(1)
+
+def lst_numbers(number):
+    removed_commas = re.sub(",", " ", number)
+    convert_lst = removed_commas.split()
+    check_if_ints(convert_lst)
+
+    lst_tuple = tuple(convert_lst)
+    print("Tuple:", lst_tuple)
+
+    lst = list(convert_lst)
+    print("List:", lst)
+    
+number = input("Sample data :")
+lst_numbers(number)
