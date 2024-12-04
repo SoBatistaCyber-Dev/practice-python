6659d1f7a2015e746ed0da7bedf45a1a32385ed5
SoBatista <72103679+SoBatista@users.noreply.github.com>
2024-12-04 21:18:00 +0100

    Solving challenge 17 (#66)
    * [Close #18] - 16 Challenge done
    
    * [Close #19] - Challenge 17 done

--- challenge_30_days/day_17.py
+++ challenge_30_days/day_17.py
@@ -1,3 +1,24 @@
 '''
     Write a Python program to check whether a file exists. 
 '''
+
+from pathlib import Path
+
+def check_if_file(current_file, given_file):
+    try:
+        return str(given_file) == str(current_file)
+    except PermissionError:
+        return False
+    
+def iterate_directory(given_file):
+    base_dir = Path('/')
+    for path in base_dir.rglob('*'):
+        try:
+            if path.is_file() and check_if_file(path, given_file):
+                return f"File found: {path}"
+        except PermissionError:
+            continue
+
+    return "File does not exist in the system"
+
+print(iterate_directory('your-files-goes-here'))
