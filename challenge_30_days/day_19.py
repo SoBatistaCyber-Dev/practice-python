7ea66521d61e79c0271244101f4aa45c5f01de02
SoBatista <72103679+SoBatista@users.noreply.github.com>
2024-12-06 11:14:37 +0100

    [Close #21] -  19 Challenge done (#67)

--- challenge_30_days/day_19.py
+++ challenge_30_days/day_19.py
@@ -1,3 +1,9 @@
 '''
     Write a Python program to locate all Python packages in the computer and lists them. 
 '''
+
+import pkg_resources
+
+installed_packages = pkg_resources.working_set
+for package in installed_packages:
+    print(package)
