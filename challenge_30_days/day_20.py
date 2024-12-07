1a115eb507681ca67b7f7fd770cf31ef559be4f0
SoBatista <72103679+SoBatista@users.noreply.github.com>
2024-12-07 10:38:21 +0100

    [Close #22] - 20 Challenge solved (#68)

--- challenge_30_days/day_20.py
+++ challenge_30_days/day_20.py
@@ -1,3 +1,9 @@
 '''
     Write a Python program to retrieve the path and name of the file currently being executed. 
 '''
+
+from pathlib import Path
+import os
+
+print("Your current working directory is:", Path.cwd())
+print("The file being executed is:", os.path.basename(__file__))
