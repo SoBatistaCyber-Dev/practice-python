fb6ac3e6796f369f3c38555c3d5c9d72886ee21e
SoBatista <72103679+SoBatista@users.noreply.github.com>
2024-12-08 23:10:02 +0100

    [Close #23] - Solving challenge 21 (#69)

--- challenge_30_days/day_21.py
+++ challenge_30_days/day_21.py
@@ -1,3 +1,15 @@
 '''
     Write a python program that does the same as ls and lists all the contents of the current folder
 '''
+
+from pathlib import Path
+
+def iterate_directory(directory):
+    if directory.is_dir():
+        for file in directory.iterdir():
+            print(file.name, end="  ")
+    else:
+        print(f"{directory} invalid")
+
+current_directory = Path.cwd()
+iterate_directory(current_directory)
