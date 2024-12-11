95eeff01c314c166f87f7b8dd2c1ed36faf2f1d2
SoBatista <solangefilipa08@gmail.com>
2024-12-11 11:21:38 +0100

    [Close #26] - 24 Challenge done

--- challenge_30_days/day_24.py
+++ challenge_30_days/day_24.py
@@ -1,3 +1,17 @@
 '''
     Write a Python program to hash a word. 
 '''
+
+import hashlib
+
+def hashing_words(word):
+    encode_as_bytes = bytes(word, 'utf-8') #Encodes the user input into UTF-8 bytes.
+    hashed_word = hashlib.sha256(encode_as_bytes).hexdigest() # hashlib.sha256()-> Processes the bytes into a hash (raw binary representation).
+    # hexdigest() -> Converts the binary hash into a hexadecimal string
+    return hashed_word
+
+print("*************************************************************")
+print("* This program will take your input and hashe it as SHA-256 *")
+print("*************************************************************")
+word = str(input("Enter a word to tranform in a hash: "))
+print(hashing_words(word))
