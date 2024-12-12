c633ac1638c35cd7b1770502146b48e7a159e24f
SoBatista <72103679+SoBatista@users.noreply.github.com>
2024-12-12 12:03:47 +0100

    #27 solving 25 challenge (#73)
    * [Close #27] - 25 Challenge done
    
    * [Close #27] - 25 Challenge done
    
    ---------
    
    Co-authored-by: SoBatista <sobatistacyber@gmail.com>

--- challenge_30_days/day_25.py
+++ challenge_30_days/day_25.py
@@ -1,3 +1,10 @@
 '''
     Write a Python program to test whether the system is a big-endian platform or a little-endian platform. 
 '''
+
+import sys
+
+if sys.byteorder == "big": # Verifies if the system's byte order (aka endianness), is big endian
+    print("The system is big-endian.")
+else:
+    print("The system is little-endian.")
