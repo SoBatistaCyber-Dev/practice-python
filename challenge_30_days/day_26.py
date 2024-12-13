0f71598a708bc2204e99047e78df6a79f90a7534
SoBatista <sobatistacyber@gmail.com>
2024-12-13 10:21:04 +0100

    [Close #28] - 26 Challenge done

--- challenge_30_days/day_26.py
+++ challenge_30_days/day_26.py
@@ -1,3 +1,14 @@
 '''
     Write a Python program that inputs a number and generates an error message if it is not a number. 
 '''
+
+def check_if_is_number(text):
+    try:
+        float(text)
+        print("That is indeed a number!")
+    except ValueError:
+        print("ERROR: That's not a number")
+
+
+text = input("Enter the number: ")
+check_if_is_number(text)
