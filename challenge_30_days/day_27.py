c0434b7718e88813eae8a4a6dc598abb606f7f69
SoBatista <72103679+SoBatista@users.noreply.github.com>
2024-12-14 21:16:24 +0100

    [Close #29] - 27 Challenge done (#75)
    Co-authored-by: SoBatista <sobatistacyber@gmail.com>

--- challenge_30_days/day_27.py
+++ challenge_30_days/day_27.py
@@ -1,3 +1,14 @@
 '''
     Write a Python program to validate an IP address.
 '''
+import ipaddress
+
+def validate_IP_address(IP):
+    try:
+        validated_ip = ipaddress.ip_address(IP) #This will verify if the inputed IP is valid or not
+        print("The IP address", validated_ip, "is valid!")
+    except ValueError:
+        print("The IP address", IP, "is invalid!")
+
+IP = input("Enter the IP to validate: ")
+validate_IP_address(IP)
