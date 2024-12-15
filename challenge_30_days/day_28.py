c3c985689451873b170b1c9df0234848d6584aa8
SoBatista <sobatistacyber@gmail.com>
2024-12-15 17:54:30 +0100

    [Close #30] - 28 Challenge done

--- challenge_30_days/day_28.py
+++ challenge_30_days/day_28.py
@@ -1,3 +1,14 @@
 '''
     Write a Python function that takes a sequence of numbers and determines whether all the numbers are different from each other. 
 '''
+
+def find_if_duplicates_on_inout(lst):
+    if not all(i.isdigit() for i in lst): # Check if all elements are numbers
+        print("That's not a number!")
+    elif len(lst) != len(set(lst)):
+        print("Not all the numbers are different!")
+    else:
+        print("All the numbers are different!")
+
+lst = input("Enter a list of numbers (with spaces between them): ").split()
+find_if_duplicates_on_inout(lst)
